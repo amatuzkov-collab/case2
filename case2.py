8a4ee6e371833cfd3d9044ccd8e49b5bc2e58a36
amatuzkov-collab <a.matuzkov@g.nsu.ru>
2026-03-02 18:57:50 +0700

    test

--- case2.py
+++ case2.py
@@ -1,1 +1,7 @@
 print('Hello world')
+
+name = input("Введите ваше имя:")
+if name == 'Andrey' or name == 'Андрей':
+    print('Чо за имя такое тупое??')
+else:
+    print('Капец у тебя имя красивое')
